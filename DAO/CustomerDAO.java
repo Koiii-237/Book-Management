92df7dc04e9092dc18a13ae2942b4e98eedcce28
Koiii-237 <koikou208@gmail.com>
2025-07-17 10:03:15 +0700

    SETUP INTERFACE

---
+++ DAO/CustomerDAO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Model.Customer;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CustomerDAO {
+//    public ArrayList<Customer> read() {
+//        
+//    }
+//
+//    public int insert(Customer cs) {
+//        
+//    }
+//
+//    public int update(Customer sp, String TheoMa) {
+//       
+//    }
+//
+//    public int delete(String maSP) {
+//        
+//    }
+}
